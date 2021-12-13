83ff7015d78043be4ddbc2da323175a51663f2b7
hyukjae <gurwo100000@gmail.com>
2021-12-13 19:55:29 +0900

    update

---
+++ learn-react/src/components/Expense/ExpenseItem/ExpenseItem.jsx
@@ -0,0 +1,16 @@
+import ExpenseDate from '../ExpenseDate/ExpenseDate';
+import './ExpenseItem.css';
+import Card from '../UI/Card';
+const ExpenseItem = ({title, date, amount}) => {
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={date} />
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${amount}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
